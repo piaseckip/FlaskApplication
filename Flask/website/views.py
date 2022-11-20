95d974765a830eaf18eb3fd76224c49dfe8e8528
Piotr Piasecki <piterpiasecki@gmail.com>
2022-11-20 02:28:46 +0100

    added logging

--- Flask/website/views.py
+++ Flask/website/views.py
@@ -1,4 +1,14 @@
-from flask import Blueprint,redirect,url_for,render_template,request,send_from_directory, jsonify,Response,Flask 
+from flask import (
+    Blueprint,
+    redirect,
+    url_for,
+    render_template,
+    request,
+    send_from_directory,
+    jsonify,
+    Response,
+    Flask,
+)
 from flask_pymongo import PyMongo, ObjectId
 from pymongo import MongoClient
 import os
@@ -7,7 +17,7 @@ from prometheus_client.core import CollectorRegistry
 from prometheus_client import Summary, Counter, Histogram, Gauge
 import time
 
-views = Blueprint('views',__name__)
+views = Blueprint("views", __name__)
 MONGODB_URI = os.environ.get("MONGODB_URI")
 client = MongoClient(str(MONGODB_URI))
 db = client.flask_db
@@ -16,119 +26,241 @@ techs = db.techs
 _INF = float("inf")
 
 graphs = {}
-graphs['c'] = Counter('python_request_operations_total', 'The total number of processed requests')
-graphs['h'] = Histogram('python_request_duration_seconds', 'Histogram for the duration in seconds.', buckets=(1, 2, 5, 6, 10, _INF))
+graphs["c"] = Counter(
+    "python_request_operations_total", "The total number of processed requests"
+)
+graphs["h"] = Histogram(
+    "python_request_duration_seconds",
+    "Histogram for the duration in seconds.",
+    buckets=(1, 2, 5, 6, 10, _INF),
+)
 
 
-@views.route('/favicon.ico')
+@views.route("/favicon.ico")
 def favicon():
+
     start = time.time()
-    graphs['c'].inc()
-    
-    time.sleep(0.600)
+    graphs["c"].inc()
+
     end = time.time()
-    graphs['h'].observe(end - start)
-    return send_from_directory(os.path.join(views.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
-    
-@views.route('/', methods=["GET"])
+    graphs["h"].observe(end - start)
+
+    return send_from_directory(
+        os.path.join(views.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
+
+
+@views.route("/", methods=["GET"])
 def home():
+
+    start = time.time()
+    graphs["c"].inc()
+
     elem_count = techs.count_documents({})
-    
+
     if elem_count <= 4:
         row_count = 1
-    elif elem_count > 4  and elem_count % 4 == 0:
-        row_count = elem_count/4
+    elif elem_count > 4 and elem_count % 4 == 0:
+        row_count = elem_count / 4
     else:
-        row_count = (elem_count//4)+1
-        
+        row_count = (elem_count // 4) + 1
+
     if elem_count <= 4:
         col_count = elem_count
     else:
         col_count = 4
-    
+
     data = techs.find()
     table = []
     for record in data:
         table.append(record)
-         
-    return render_template("index.html",ec=elem_count,rc=row_count,data=table,cc=col_count)
 
-@views.route('/adding', methods=["POST","GET"])
+    end = time.time()
+    graphs["h"].observe(end - start)
+
+    return render_template(
+        "index.html", ec=elem_count, rc=row_count, data=table, cc=col_count
+    )
+
+
+@views.route("/adding", methods=["POST", "GET"])
 def adding():
-    if request.method=='POST':
-        name = request.form['tech_name']
-        descr = request.form['tech_descr']
-        link = request.form['image_link']
-        if techs.count_documents({'name': name}) != 0 :
-            return redirect(url_for('views.action', title="Adding",header="We can't perform that action",paragraph="Record with that name exists."))
+
+    start = time.time()
+    graphs["c"].inc()
+
+    if request.method == "POST":
+        name = request.form["tech_name"]
+        descr = request.form["tech_descr"]
+        link = request.form["image_link"]
+        if techs.count_documents({"name": name}) != 0:
+
+            end = time.time()
+            graphs["h"].observe(end - start)
+
+            return redirect(
+                url_for(
+                    "views.action",
+                    title="Adding",
+                    header="We can't perform that action",
+                    paragraph="Record with that name exists.",
+                )
+            )
         else:
-            techs.insert_one({'name': name, 'descr': descr, 'link': link})
-            return redirect(url_for('views.action', title="Adding",header="Element succesfully added",paragraph="You can check it on the home page!"))
-    elif request.method=='GET':
+            techs.insert_one({"name": name, "descr": descr, "link": link})
+            end = time.time()
+            graphs["h"].observe(end - start)
+            return redirect(
+                url_for(
+                    "views.action",
+                    title="Adding",
+                    header="Element succesfully added",
+                    paragraph="You can check it on the home page!",
+                )
+            )
+    elif request.method == "GET":
+        end = time.time()
+        graphs["h"].observe(end - start)
         return render_template("adding.html")
-    
-@views.route('/deleting', methods=["POST","GET"])
+
+
+@views.route("/deleting", methods=["POST", "GET"])
 def deleting():
-    if request.method=='POST':
-        name = request.form['tech_name']
-        if techs.count_documents({'name': name}) != 0 :
-            techs.delete_one({'name': name})
-            return redirect(url_for('views.action',title="Delete",header="Element succesfully deleted",paragraph="You can check it on the home page!"))
+    start = time.time()
+    graphs["c"].inc()
+    if request.method == "POST":
+        name = request.form["tech_name"]
+        if techs.count_documents({"name": name}) != 0:
+            techs.delete_one({"name": name})
+            end = time.time()
+            graphs["h"].observe(end - start)
+            return redirect(
+                url_for(
+                    "views.action",
+                    title="Delete",
+                    header="Element succesfully deleted",
+                    paragraph="You can check it on the home page!",
+                )
+            )
         else:
-            return redirect(url_for('views.action',title="Delete",header="There is no element with that name!",paragraph="Check if u provided correct name"))
-    elif request.method=='GET':
+            end = time.time()
+            graphs["h"].observe(end - start)
+            return redirect(
+                url_for(
+                    "views.action",
+                    title="Delete",
+                    header="There is no element with that name!",
+                    paragraph="Check if u provided correct name",
+                )
+            )
+    elif request.method == "GET":
+        end = time.time()
+        graphs["h"].observe(end - start)
         return render_template("deleting.html")
-    
-@views.route('/edit', methods=["POST","GET"])
+
+
+@views.route("/edit", methods=["POST", "GET"])
 def edit():
-    if request.method=='POST':
-        name = request.form['tech_name']
-        if techs.count_documents({'name': name}) != 0 :
-            data = techs.find({'name': name})
+    start = time.time()
+    graphs["c"].inc()
+    if request.method == "POST":
+        name = request.form["tech_name"]
+        if techs.count_documents({"name": name}) != 0:
+            data = techs.find({"name": name})
             table = []
             for record in data:
                 table.append(record)
-            return redirect(url_for('views.editing',id=table[0]["_id"],name=table[0]["name"],descr=table[0]["descr"],link=table[0]["link"]))
+            end = time.time()
+            graphs["h"].observe(end - start)
+            return redirect(
+                url_for(
+                    "views.editing",
+                    id=table[0]["_id"],
+                    name=table[0]["name"],
+                    descr=table[0]["descr"],
+                    link=table[0]["link"],
+                )
+            )
         else:
-            return redirect(url_for('views.action',title="Edit",header="There is no element with that name!",paragraph="Check if u provided correct name"))
-    elif request.method=='GET':
+            end = time.time()
+            graphs["h"].observe(end - start)
+            return redirect(
+                url_for(
+                    "views.action",
+                    title="Edit",
+                    header="There is no element with that name!",
+                    paragraph="Check if u provided correct name",
+                )
+            )
+    elif request.method == "GET":
+        end = time.time()
+        graphs["h"].observe(end - start)
         return render_template("edit.html")
 
-@views.route('/editing', methods=["POST","GET"])
+
+@views.route("/editing", methods=["POST", "GET"])
 def editing():
-    if request.method=='POST':
+    start = time.time()
+    graphs["c"].inc()
+    if request.method == "POST":
         id = request.form["id"]
         name = request.form["name"]
         descr = request.form["descr"]
         link = request.form["link"]
-        techs.find_one_and_update({'_id': ObjectId(id) },{'$set': {'name': name, 'descr': descr,'link': link}})
-        return redirect(url_for('views.action', title="Edit",header="Element succesfully edited",paragraph="You can check it on the home page!"))
-    elif request.method=='GET':
-        
+        techs.find_one_and_update(
+            {"_id": ObjectId(id)},
+            {"$set": {"name": name, "descr": descr, "link": link}},
+        )
+        end = time.time()
+        graphs["h"].observe(end - start)
+        return redirect(
+            url_for(
+                "views.action",
+                title="Edit",
+                header="Element succesfully edited",
+                paragraph="You can check it on the home page!",
+            )
+        )
+    elif request.method == "GET":
+
         id = request.args["id"]
         name = request.args["name"]
         descr = request.args["descr"]
         link = request.args["link"]
-        return render_template("editing.html", id=id,name=name,descr=descr,link=link)
-        
-    
-@views.route('/action')
-def action():
-    title=request.args['title']
-    header=request.args['header']
-    paragraph=request.args['paragraph']
-    return render_template("action.html", t=title,h=header,p=paragraph)
+        end = time.time()
+        graphs["h"].observe(end - start)
+        return render_template("editing.html", id=id, name=name, descr=descr, link=link)
 
-@views.route('/tech')
+
+@views.route("/action")
+def action():
+    start = time.time()
+    graphs["c"].inc()
+    title = request.args["title"]
+    header = request.args["header"]
+    paragraph = request.args["paragraph"]
+    end = time.time()
+    graphs["h"].observe(end - start)
+    return render_template("action.html", t=title, h=header, p=paragraph)
+
+
+@views.route("/tech")
 def tech():
-    title=request.args['h']
-    descr=request.args['des']
-    image=request.args['img']
-    return render_template("tech.html", h=title,des=descr,img=image)
+    start = time.time()
+    graphs["c"].inc()
+    title = request.args["h"]
+    descr = request.args["des"]
+    image = request.args["img"]
+    end = time.time()
+    graphs["h"].observe(end - start)
+    return render_template("tech.html", h=title, des=descr, img=image)
+
 
 @views.route("/metrics")
 def requests_count():
     res = []
-    for k,v in graphs.items():
+    for k, v in graphs.items():
         res.append(prometheus_client.generate_latest(v))
     return Response(res, mimetype="text/plain")
